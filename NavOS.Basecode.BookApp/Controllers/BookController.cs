11de2f6224766859e7c63ba64740162bd28143cf
Jay Mar <jaymarrebanda123@gmail.com>
2023-10-22 23:26:45 +0800

    test push

---
+++ NavOS.Basecode.BookApp/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NavOS.Basecode.BookApp.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("ViewBooks");
+        }
+        public IActionResult ViewBooks()
+        {
+            return View("ViewSingleBook");
+        }
+    }
+}
