4377adadf6e9f1674622462be4bd992215356bb9
Jay Mar <jaymarrebanda123@gmail.com>
2023-11-30 11:49:38 +0800

    done fixing book service/controller logical errors

---
+++ NavOS.Basecode.Services/ServiceModels/BookGenreViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavOS.Basecode.Services.ServiceModels
+{
+    public class BookGenreViewModel
+    {
+        public BookViewModel Book { get; set; }
+        public List<GenreViewModel> Genres { get; set; }
+    }
+}
